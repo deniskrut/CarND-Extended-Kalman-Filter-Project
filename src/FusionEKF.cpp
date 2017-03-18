eb620b3275aa139288ee5a7adce8fab5068de7cc
Denis Krut <denis.krut@gmail.com>
2017-03-17 20:56:57 -0700

    Update step for Radar and Laser
    feat: Perform the Update step for Radar and Laser

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -97,16 +97,12 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    *  Update
    ****************************************************************************/
 
-  /**
-   TODO:
-     * Use the sensor type to perform the update step.
-     * Update the state and covariance matrices.
-   */
-
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
